657fa164e21917b5ffad8ee05418a46b00d7e98d
Renato Corbellini <renacorbe@gmail.com>
2023-03-28 12:17:09 -0300

    Update logout redirect url

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
@@ -7,7 +8,5 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  document = document.location.origin;
-
-  constructor(public auth: AuthService) {}
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
 }
